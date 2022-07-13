2b400d4d2b6685b884fb0c406b856f1732e167e4
RyotaroIshii <108617872+RyotaroIshii@users.noreply.github.com>
2022-07-13 18:50:44 +0000

    [ADD] 保存機能と一覧機能＆投稿と一覧機能を同ページに

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,12 +3,25 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books/show'
+  end
+
   def index
+    @books = Book.all
   end
 
   def show
   end
 
   def edit
+  end
+
+
+  private
+  def book_params
+    params.permit(:title, :body)
   end
 end
