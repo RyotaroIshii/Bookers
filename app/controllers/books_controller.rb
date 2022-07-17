e24fa21a9703ad98acc067e338d0531bdf33cdec
RyotaroIshii <108617872+RyotaroIshii@users.noreply.github.com>
2022-07-17 11:12:53 +0000

    [add] createとupdateのフラッシュメッセージ機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,17 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
-
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    else
+      render "index"
+    end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -23,8 +24,10 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    end
   end
 
   def destroy
