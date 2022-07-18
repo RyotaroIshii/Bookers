1bac36a4060a5838f2b847722f64e4f2f9b46fd3
RyotaroIshii <108617872+RyotaroIshii@users.noreply.github.com>
2022-07-18 10:23:28 +0000

    [add] バリテーションによる空欄投稿時のエラー表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,12 @@
 class BooksController < ApplicationController
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
-      render "index"
+      @books = Book.all
+      render :index
     end
   end
 
